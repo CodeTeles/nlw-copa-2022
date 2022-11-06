54d9db70f1280fbcc1c730cb30919c023bd8c040
CodTeles <lucastelessp@outlook.com>
2022-11-06 20:03:12 -0300

    assets

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
-    <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="Brandeira da ${player2}" />
+        <img src="assets/icon-${player1}.svg" alt="Imagem da bandeira de ${player1}">
+        <strong>${hour}</strong>
+        <img src="assets/icon-${player2}.svg" alt="Imagem da bandeira de ${player2}">
     </li>
+    <div class='names'>
+      <h1>${player1}</h1>
+      <h1>${player2}</h1>
+    </div>
   `
 }
 
-let delay = -0.3
-
+let delay = -0.1
 function createCard(date, day, games) {
-  delay = delay + 0.3
-  return `
-    <div class="card" style="animation-delay: ${delay}s">
-    <h2>${date} <span>${day}</span></h2>
-     <ul>
-     ${games}
-    </ul>
-  </div>
-  `
+  delay = delay + 0.1
+  return `   
+      <div class="card" style="animation-delay: ${delay}s">
+        <h2>${date}<span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+      </div>
+    `
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "Quinta", createGame("brazil", "16:00", "serbia")) +
   createCard(
-    "28/11",
-    "Segunda",
-    createGame("brazil", "13:00", "switzerland") +
-      createGame("portugal", "16:00", "uruguai")
+    "22/11",
+    "terça",
+    createGame("argentina", "16:00", "saudi arabia")
   ) +
-  createCard("02/12", "Sexta", createGame("cameroon", "16:00", "brazil"))
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard("26/11", "sabado", createGame("argentina", "16:00", "mexico")) +
+  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
+  createCard("30/11", "quarta", createGame("poland", "16:00", "argentina")) +
+  createCard("02/12", "sexta", createGame("cameroon", "16:00", "brazil"))
+
+let changeColor = document.querySelector("body")
+
+function removeColor(color) {
+  changeColor.classList.remove(color)
+}
+
+function addColor(color) {
+  changeColor.classList.add(color)
+}
+
+document.querySelector("#yellow").addEventListener("click", function () {
+  removeColor("green")
+  removeColor("blue")
+})
+document.querySelector("#blue").addEventListener("click", function () {
+  removeColor("green")
+  addColor("blue")
+})
+document.querySelector("#green").addEventListener("click", function () {
+  addColor("green")
+})
